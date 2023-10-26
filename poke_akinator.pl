ad483d01ff2c0be9b049a11b148b4a7c75c7c82d
João Victor Batista <jvtexbat@hotmail.com>
2023-10-25 22:13:08 -0300

    Cria mock dos predicados

---
+++ poke_akinator.pl
@@ -0,0 +1,58 @@
+pokemon(bulbasaur).
+pokemon(ivysaur).
+pokemon(venasaur).
+pokemon(charmander).
+pokemon(charmeleon).
+pokemon(charizard).
+pokemon(ponita).
+pokemon(rapidash).
+pokemon(mewtwo).
+
+cor(bulbasaur, green).
+cor(ivysaur, green).
+cor(venasaur, green).
+cor(charmander, orange).
+cor(charmeleon, orange).
+cor(charizard, orange).
+cor(ponita, white).
+cor(rapidash, white).
+cor(mewtwo, white).
+
+tipo(bulbasaur, grass).
+tipo(ivysaur, grass).
+tipo(venasaur, grass).
+tipo(bulbasaur, poison).
+tipo(ivysaur, poison).
+tipo(venasaur, poison).
+tipo(charmander, fire).
+tipo(charmeleon, fire).
+tipo(charizard, fire).
+tipo(charizard, flying).
+tipo(ponita, fire).
+tipo(rapidash, fire).
+tipo(mewtwo, psychic).
+
+forma(bulbasaur, quadruped).
+forma(ivysaur, quadruped).
+forma(venasaur, quadruped).
+forma(charmander, bipede).
+forma(charmeleon, bipede).
+forma(charizard, bipede).
+forma(ponita, quadruped).
+forma(rapidash, quadruped).
+forma(mewtwo, bipede).
+
+inicial(bulbasaur).
+inicial(ivysaur).
+inicial(venasaur).
+inicial(charmander).
+inicial(charmeleon).
+inicial(charizard).
+
+lendario(mewtwo).
+
+evolui_para(bulbasaur, ivysaur).
+evolui_para(ivysaur, venasaur).
+evolui_para(charmander, charmeleon).
+evolui_para(charmeleon, charizard).
+evolui_para(ponita, rapidash).
